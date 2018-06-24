43606980626530e04b18a053c4d18a165d40d477
Alexandros Leontakianakos <alleon20@gmail.com>
2018-06-24 22:53:00 +0300

    Working basic eventhub

--- dist/eventhub.js
+++ dist/eventhub.js
@@ -1,2 +1,26 @@
 "use strict";
-// This will be where the magic happens... hopefully
+class EventHub {
+    constructor() {
+        this.handler = {};
+        this.$on = this.on;
+        this.listen = this.on;
+        this.$off = this.off;
+        this.remove = this.off;
+        this.$emit = this.emit;
+        this.fire = this.emit;
+    }
+    on(event, fn) {
+        this.handler[event] = fn;
+    }
+    off(event) {
+        delete this.handler[event];
+        if (this.handler[event] != undefined) {
+            this.handler[event] == undefined;
+        }
+    }
+    emit(event, ...a) {
+        if (typeof this.handler[event] === "function") {
+            this.handler[event](...a);
+        }
+    }
+}
--- src/eventhub.ts
+++ src/eventhub.ts
@@ -1,1 +1,32 @@
-// This will be where the magic happens... hopefully
+interface EventHubConfig {}
+
+class EventHub {
+
+    private handler: { [key: string]: Function } = {};
+
+    $on: Function = this.on;
+    listen: Function = this.on;
+
+    $off: Function = this.off;
+    remove: Function = this.off;
+    
+    $emit: Function = this.emit;
+    fire: Function = this.emit;
+
+    on (event: string, fn: Function): void {
+        this.handler[event] = fn;
+    }
+
+    off (event: string) {
+        delete this.handler[event];
+        if (this.handler[event] != undefined) {
+            this.handler[event] == undefined;
+        }
+    }
+
+    emit (event: string, ...a: any[]) {
+        if (typeof this.handler[event] === "function") {
+            this.handler[event](...a);
+        }
+    }
+}
