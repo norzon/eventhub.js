e2239616b8ffcb504663fbba04d0f92c00965d0d
Alexandros Leontakianakos <alleon20@gmail.com>
2018-07-19 21:38:06 +0300

    Implement #3

--- dist/eventhub.js
+++ dist/eventhub.js
@@ -1,26 +1,45 @@
-"use strict";
 class EventHub {
     constructor() {
-        this.handler = {};
         this.$on = this.on;
         this.listen = this.on;
         this.$off = this.off;
         this.remove = this.off;
         this.$emit = this.emit;
         this.fire = this.emit;
+        /**
+         * Stores the callback functions in an object,
+         * in order to be used later, after an emit event.
+         */
+        this.handler = {};
     }
+    /**
+     * Attach the given function to the key specified.
+     * @param {string} event The unique key
+     * @param {Function} fn The callback function to execute
+     */
     on(event, fn) {
         this.handler[event] = fn;
     }
+    /**
+     * Removes a handler that was specified using the "on" function,
+     * or one of it's aliases
+     * @param {string} event The unique key previously given by calling the "on" function
+     */
     off(event) {
         delete this.handler[event];
         if (this.handler[event] != undefined) {
             this.handler[event] == undefined;
         }
     }
-    emit(event, ...a) {
+    /**
+     * Calls the function identified by the key and passes
+     * the arguments to the function
+     * @param event The unique key used to attach an event
+     * @param args The arguments to pass to the callback function
+     */
+    emit(event, ...args) {
         if (typeof this.handler[event] === "function") {
-            this.handler[event](...a);
+            this.handler[event](...args);
         }
     }
 }
--- src/eventhub.ts
+++ src/eventhub.ts
@@ -1,32 +1,50 @@
-interface EventHubConfig {}
-
 class EventHub {
 
+    $on: EventHub["on"] = this.on;
+    listen: EventHub["on"] = this.on;
+    
+    $off: EventHub["off"] = this.off;
+    remove: EventHub["off"] = this.off;
+    
+    $emit: EventHub["emit"] = this.emit;
+    fire: EventHub["emit"] = this.emit;
+
+    /**
+     * Stores the callback functions in an object,
+     * in order to be used later, after an emit event.
+     */
     private handler: { [key: string]: Function } = {};
 
-    $on: Function = this.on;
-    listen: Function = this.on;
-
-    $off: Function = this.off;
-    remove: Function = this.off;
-    
-    $emit: Function = this.emit;
-    fire: Function = this.emit;
-
+    /**
+     * Attach the given function to the key specified.
+     * @param {string} event The unique key
+     * @param {Function} fn The callback function to execute
+     */
     on (event: string, fn: Function): void {
         this.handler[event] = fn;
     }
 
-    off (event: string) {
+    /**
+     * Removes a handler that was specified using the "on" function,
+     * or one of it's aliases
+     * @param {string} event The unique key previously given by calling the "on" function
+     */
+    off (event: string): void {
         delete this.handler[event];
         if (this.handler[event] != undefined) {
             this.handler[event] == undefined;
         }
     }
 
-    emit (event: string, ...a: any[]) {
+    /**
+     * Calls the function identified by the key and passes
+     * the arguments to the function
+     * @param event The unique key used to attach an event
+     * @param args The arguments to pass to the callback function
+     */
+    emit (event: string, ...args: any[]): void {
         if (typeof this.handler[event] === "function") {
-            this.handler[event](...a);
+            this.handler[event](...args);
         }
     }
 }
